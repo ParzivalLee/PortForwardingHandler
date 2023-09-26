0c8511b107b175a40b62f47f9552b6d8670cb6f1
Parzival Lee <parzivallee@163.com>
2023-09-26 10:54:54 +0800

    添加了UDP转发，服务器的初始化将在程序运行时进行

--- PortForwardingHandler.py
+++ PortForwardingHandler.py
@@ -15,35 +15,62 @@ class PortForwardingHandler:
     def __init__(self, target_port: int, src_address: tuple):
         """
         端口转发控制器
-        :param target_port: 目标端口
-        :param src_address: 源地址
+        :param target_port: 目标端口（计算机将开放这个端口用于监听）
+        :param src_address: 源地址（计算机会将数据转发到这个地址）
         """
-        self.targetPort: int = target_port
-        self.srcAddress: tuple = src_address
-        self.server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(('0.0.0.0', self.targetPort))
-        self.server.listen()
+        # 基础服务器变量
+        self.targetHost: str = '0.0.0.0'  # 目标host
+        self.targetPort: int = target_port  # 目标端口
+        self.srcAddress: tuple = src_address  # 源地址，数据将转发到这里
+        # 缓冲区大小
+        self.tcpBufferSize: int = 1024  # TCP接收的数据包的大小
+        self.udpBufferSize: int = 1024  # UDP接收的数据包的大小
+
+        # 初始化TCP转发服务器
+        self.tcpServer: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 初始化UDP转发服务器
+        self.udpServer: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         self.sockets = []  # 全部的套接字
         self.isAlive: bool = True
 
-    def listenHandler(self):
+    def tcpListeningHandler(self) -> None:
+        """
+        TCP转发监听控制器
+        :return: None
+        """
         while self.isAlive:
             try:
-                client, addr = self.server.accept()
+                client, addr = self.tcpServer.accept()
                 self.sockets.append(client)
                 src_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.sockets.append(src_socket)
                 src_socket.connect(self.srcAddress)
-                threading.Thread(target=self.resendHandler, args=(src_socket, client)).start()
-                threading.Thread(target=self.resendHandler, args=(client, src_socket)).start()
+                threading.Thread(target=self.tcpResendHandler, args=(src_socket, client)).start()
+                threading.Thread(target=self.tcpResendHandler, args=(client, src_socket)).start()
             except OSError:
                 break
 
-    def resendHandler(self, send_socket: socket.socket, recv_socket: socket.socket) -> None:
+    def udpListeningHandler(self) -> None:
         """
-        数据转发控制器
+        UDP转发监听控制器
+        :return: None
+        """
+        while self.isAlive:
+            try:
+                data, addr0 = self.udpServer.recvfrom(1024)
+                self.udpServer.sendto(data, self.srcAddress)
+                data, addr1 = self.udpServer.recvfrom(1024)
+                self.udpServer.sendto(data, addr0)
+            except Exception as e:
+                print(e)
+
+    def tcpResendHandler(self, send_socket: socket.socket, recv_socket: socket.socket) -> None:
+        """
+        TCP数据转发控制器
         :param send_socket: 发送数据的socket
         :param recv_socket: 接收数据的socket
+        :return: None
         """
         try:
             s_buffer = recv_socket.recv(1024)
@@ -59,11 +86,31 @@ class PortForwardingHandler:
             send_socket.close()
             recv_socket.close()
 
-    def start(self):
-        threading.Thread(target=self.listenHandler).start()
+    def start(self) -> None:
+        """
+        开启端口转发
+        :return: None
+        """
+        # 初始化TCP转发服务器
+        self.tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcpServer.bind((self.targetHost, self.targetPort))
+        self.tcpServer.listen()
+        # 初始化UDP转发服务器
+        self.udpServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udpServer.bind((self.targetHost, self.targetPort))
 
-    def stop(self):
+        # 开启服务器
+        threading.Thread(target=self.tcpListeningHandler).start()
+        threading.Thread(target=self.udpListeningHandler).start()
+
+    def stop(self) -> None:
+        """
+        关闭端口转发
+        :return: None
+        该功能存在一定的延迟
+        """
         for i in self.sockets:
             i.close()
-        self.server.close()
+        self.tcpServer.close()
+        self.udpServer.close()
         self.isAlive = False
